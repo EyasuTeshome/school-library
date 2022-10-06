ffa4c8dba66315146f2ca4567efb53bba17af8c0
Josh <joshtesh91@mail.com>
2022-10-06 22:29:53 +0800

    fixed comments geven by code reviewer

--- school/decorate.rb
+++
@@ -1,5 +0,0 @@
-class Nameable
-  def correct_name(name)
-    name.capitalize![0..9]
-  end
-end
--- school/person.rb
+++ school/person.rb
@@ -1,22 +1,21 @@
-require_relative 'decorate'
-class Person
-  attr_accessor :name, :age
+require_relative 'nameable'
+class Person < Nameable
+  attr_accessor :name, :age, :parent_permission
   attr_reader :id
 
-  def initialize(age, name: 'unknown', parent_permission: true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @nameable = Nameable.new
   end
 
   def age_verify
     of_age? || @parent_permission
   end
 
-  def validate_name
-    @name = @nameable.correct_name(@name)
+  def correct_name
+    @name
   end
 
   private
