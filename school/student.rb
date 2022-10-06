cb5503431a4ebc4f90b028affd81ce60e612c296
Josh <joshtesh91@mail.com>
2022-10-07 02:43:46 +0800

    fix linter

--- school/student.rb
+++ school/student.rb
@@ -7,6 +7,7 @@ class Student < Person
   def play_hooky
     "¯\(ツ)/¯"
   end
+
   def classroom=(classroom)
     @classroom = classroom
     classroom.student.push(self) unless classroom.students.include?(self)
