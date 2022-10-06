03decc7f64190ee5bf4139b51a986a6ebd78a558
Josh <joshtesh91@mail.com>
2022-10-07 02:41:11 +0800

    update student

--- school/student.rb
+++ school/student.rb
@@ -7,4 +7,8 @@ class Student < Person
   def play_hooky
     "¯\(ツ)/¯"
   end
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.students.include?(self)
+  end
 end
